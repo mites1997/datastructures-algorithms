203139af0d1009e58a6bb5c53abac53a89e21968
mites1997 <arnabfeb15@gmail.com>
2023-01-15 12:31:36 +0530

    Modify. the binary addition algorithm

--- cormen/BinaryAddition.java
+++ cormen/BinaryAddition.java
@@ -4,24 +4,21 @@ import java.util.Arrays;
 
 public class BinaryAddition {
     public static void main(String[] args) {
-        int [] arr = addBinaryNumbers(new int[]{1,1,0},new int[]{1,1,0});
-        for (int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
-        }
-
-
+        int[] arr = addBinaryNumbers(new int[]{1, 0, 1}, new int[]{0, 0, 1});
+        System.out.println(Arrays.toString(arr));
     }
-    public static int [] addBinaryNumbers(int a[],int b[])
-    {
-        int n =Integer.max(a.length, b.length);
-        int c[]=new int[n+1];
-        int carry =0;
-        for(int i=0;i<n;i++)
-        {
-            c[n-i-1]=(a[i]+b[i]+carry)%2;
-            carry= (a[i]+b[i]+carry) /2;
+
+    public static int[] addBinaryNumbers(int a[], int b[]) {
+        int n = Integer.max(a.length, b.length);
+        int c[] = new int[n + 1];
+        int j = n;
+        int carry = 0;
+        for (int i = n-1; i >=0; i--) {
+            c[j] = (a[i] + b[i] + carry) % 2;
+            carry = (a[i] + b[i] + carry) / 2;
+            j--;
         }
-        c[n]=carry;
+            c[j] = carry;
         return c;
     }
 }
